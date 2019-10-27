f947f8d07d7302133505b66798992d616a43ea9e
ferles <ferles@kth.se>
2019-10-27 22:51:18 +0100

    minor code fix

--- src/PyTorch/utils.py
+++ src/PyTorch/utils.py
@@ -169,7 +169,7 @@ def kd_att_loss(student_outputs, teacher_outputs, labels, T=4, a=0.9, b=1000, cr
 
     loss_term1 = (1-a) * criterion1(student_out, labels)
     # changed to log softmax for student_out and 2a for loss_term2 after inspection of the official code
-    loss_term2 = criterion2(F.log_softmax(student_out/T), F.softmax(teacher_out/T))
+    loss_term2 = criterion2(F.log_softmax(student_out/T, dim=1), F.softmax(teacher_out/T), dim=1)
     loss_term2 *= (T**2)*2*a
     attention_losses = [attention_loss(att1, att2) for (att1, att2) in activation_pairs]
     loss_term3 = b * sum(attention_losses)
