8c5e9e79d9f8be661630095b824334f1d71a6e8a
ferles <ferles@kth.se>
2019-10-27 23:22:26 +0100

    upd

--- src/PyTorch/train_kd_att.py
+++ src/PyTorch/train_kd_att.py
@@ -95,7 +95,7 @@ def train(args):
     if log:
         teacher_str = "WideResNet-{}-{}".format(wrn_depth_teacher, wrn_width_teacher)
         student_str = "WideResNet-{}-{}".format(wrn_depth_student, wrn_width_student)
-        logfile = "Teacher-{}-Student-{}-{}-M={}".format(teacher_str, student_str, kd_att_configurations.dataset, M)
+        logfile = "./Teacher-{}-Student-{}-{}-M={}".format(teacher_str, student_str, kd_att_configurations.dataset, M)
         with open("{}.txt".format(logfile), "w") as temp:
             temp.write('KD_ATT with teacher {} and student {} in {} with M={}\n'.format(teacher_str, student_str, kd_att_configurations.dataset, M))
     else:
