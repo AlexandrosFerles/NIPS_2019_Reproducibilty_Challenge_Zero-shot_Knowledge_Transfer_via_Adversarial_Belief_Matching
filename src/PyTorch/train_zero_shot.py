c4733e1b44cfc03e13aec458b9e76f3d035638b0
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-30 21:12:54 +0000

    upd

--- src/PyTorch/train_zero_shot.py
+++ src/PyTorch/train_zero_shot.py
@@ -161,7 +161,7 @@ def train(args):
     checkpoint = bool(kd_att_configurations.checkpoint)
 
     if torch.cuda.is_available():
-        device = torch.device('cuda:3')
+        device = torch.device('cuda:0')
     else:
         device = torch.device('cpu')
 
